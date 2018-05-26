57ed139bac2f280f35f9aa18e0df89ac4d6d684c
rxrossi <rx.rossi@gmail.com>
2018-05-26 15:38:25 -0300

    Pedido, Pagamento, EstadoPagamento

--- src/main/java/com/rxrossi/cursomc/domain/Estado.java
+++ src/main/java/com/rxrossi/cursomc/domain/Estado.java
@@ -15,18 +15,18 @@ import com.fasterxml.jackson.annotation.JsonBackReference;
 @Entity
 public class Estado implements Serializable {
 	private static final long serialVersionUID = 1L;
-	
+
 	@Id
-	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String nome;
-	
+
 	@JsonBackReference
-	@OneToMany(mappedBy="estado")
+	@OneToMany(mappedBy = "estado")
 	private List<Cidade> cidades = new ArrayList<>();
-	
+
 	public Estado() {
-		
+
 	}
 
 	public Estado(Integer id, String nome) {
@@ -83,6 +83,5 @@ public class Estado implements Serializable {
 			return false;
 		return true;
 	}
-	
-	
+
 }
