69ebf8c4399fc8cf339c69abcda4b23484066144
rxrossi <rx.rossi@gmail.com>
2018-05-29 21:16:33 -0300

    Validacao customizada: email nao repetido na atualizacao de Cliente

--- src/main/java/com/rxrossi/cursomc/dto/ClienteDTO.java
+++ src/main/java/com/rxrossi/cursomc/dto/ClienteDTO.java
@@ -8,7 +8,9 @@ import javax.validation.constraints.NotEmpty;
 import org.hibernate.validator.constraints.Length;
 
 import com.rxrossi.cursomc.domain.Cliente;
+import com.rxrossi.cursomc.services.validation.ClienteUpdate;
 
+@ClienteUpdate
 public class ClienteDTO implements Serializable {
 	private static final long serialVersionUID = 1L;
 
